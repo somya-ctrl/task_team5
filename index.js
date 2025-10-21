286d3c59fcc1cce1f8de6638785f521099bf17da
somya-ctrl <agarwalsomya224@gmail.com>
2025-10-21 15:39:22 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express= require('express');
-
+import cors from "cors";
 require('dotenv').config();
 const mongoose = require('mongoose');
 const {connectmongoDB} = require('./connect');
 const userRoutes = require('./routes/user');
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173", // allow your local frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-import cors from "cors";
-app.use(cors({ origin: "http://localhost:5173" }));
+
+
 
 const PORT = process.env.PORT;
 app.use('/', userRoutes);
