8e0c7ad80a58fab0145fc7ccfd2245a239839f5d
harshita10sharma <harshita25404@gmail.com>
2025-10-30 23:22:20 +0530

    Update main.py

--- MentalHealthAPI/main.py
+++ MentalHealthAPI/main.py
@@ -114,9 +114,9 @@ def predict(data: InputData):
 
 
 # ------------------------------
-# Run app (for local debugging)
+# Run app (for Render or local)
 # ------------------------------
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app:app", host="127.0.0.1", port=port, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Render provides PORT automatically
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
